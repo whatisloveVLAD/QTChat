004e70c289d77cecd5a30fdcc25838022fa4b7a2
Vladislav Evseviev <vyevsevev@bk.ru>
2025-03-18 14:15:08 +0300

    Создание проекта

---
+++ build/Desktop-Debug/CMakeFiles/chat.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chat_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/chat_autogen.dir/ParseCache.txt"
+  "chat_autogen"
+  "CMakeFiles/chat.dir/chat_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/chat.dir/chat_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/chat.dir/main.cpp.o"
+  "CMakeFiles/chat.dir/main.cpp.o.d"
+  "CMakeFiles/chat.dir/mainwindow.cpp.o"
+  "CMakeFiles/chat.dir/mainwindow.cpp.o.d"
+  "chat"
+  "chat.pdb"
+  "chat_autogen/mocs_compilation.cpp"
+  "chat_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
